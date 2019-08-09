93d8fd00070ab1f0a60dce8d4d95a4f2ee9549d6
turbobots11 <53749152+turbobots11@users.noreply.github.com>
2019-08-09 12:16:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "$"
-var adminprefix = '$'
+var prefix = "!"
+var adminprefix = '!'
 
 
 //bc
@@ -23,7 +23,7 @@ client.on("message", message => {
 //bc online
 
 
-  var prefix = "$";
+  var prefix = "!";
 
   client.on("message", message => {
   
